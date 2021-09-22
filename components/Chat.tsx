3d7230d0798b053498e3fe7ff494137655f2eb83
azeez1776 <aboud3am81@gmail.com>
2021-09-23 00:18:39 +0300

    added components

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { View, Text } from 'react-native'
-import {ChatItem} from './ChatItem'
+import { View, Text, FlatList, Button, TextInput, ListRenderItem } from 'react-native'
+import {ChatItem, RenderChatItem} from './ChatItem'
+import Styles from './Styles'
 
 type ChatProps = {
     username:string;
@@ -12,9 +13,42 @@ const Chat = ({username, image}: ChatProps) => {
     const [chatInput, setChatInput] = useState('');
     const [chatItemList, setChatItemList] = useState<ChatItem[]>([]);
 
+    const renderItem: ListRenderItem<ChatItem> = ({ item }) => (
+        <RenderChatItem chatItem={item} username={username}></RenderChatItem>
+      );
+
     return (
         <View style={{marginTop:200, alignSelf:'center'}}>
-            <Text>Hello {username} just seconds to start chatting</Text>
+           <FlatList
+        inverted
+        data={chatItemList.sort((a, b) => b.timeStamp - a.timeStamp)}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      ></FlatList>
+
+      <View style={Styles.sendSection}>
+        <TextInput
+          style={Styles.chatTextInput}
+          value={chatInput}
+          onChangeText={(text) => setChatInput(text)}
+        ></TextInput>
+        <Button
+          title="Send"
+          onPress={() => {
+            setChatItemList([
+              ...chatItemList,
+              {
+                id: Math.random().toString(36).substring(7),
+                text: chatInput,
+                image: image,
+                timeStamp: Date.now(),
+                by: username,
+              },
+            ]);
+            setChatInput("");
+          }}
+        ></Button>
+      </View>
         </View>
     )
 }
